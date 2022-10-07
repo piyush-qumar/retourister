8f01e5a8cb91a3d205e1bb1697bd286628f9950f
piyush_qumar <piyushkumar5271@gmail.com>
2022-10-07 23:22:04 +0530

    added some changes in the tour route file

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -4,8 +4,8 @@ const userController=require("./../controllers/userController")
 const authController=require("./../controllers/authController")
 router.post('/signup',authController.signup);
 router.post('/login',authController.login);
-router.post('/forgotPassword',authController.login);
-router.post('/resetPassword',authController.login);
+router.post('/forgotPassword',authController.forgotPassword);
+router.post('/resetPassword',authController.resetPassword);
 
 
 router.route("/").get(userController.getAllUsers).post(userController.createUser);
