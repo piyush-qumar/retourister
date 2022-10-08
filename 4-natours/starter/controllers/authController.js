a1cd3ae7a8021ab4c4efdd7506dee6dbaae23a30
piyush_qumar <piyushkumar5271@gmail.com>
2022-10-08 21:15:47 +0530

    forgetpassword router defined in authcontroller  and in userroutes patch was added and email.js file was added

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,8 +1,10 @@
 const { promisify }=require('util');
+const crypto=require('crypto');
 const User=require(".//..//models///userModel");
 const catchAsync=require(".///../////utils/////catchAsync");
 const jwt=require("jsonwebtoken");
 const AppError=require(".//..//utils///AppError");
+const sendEmail=require(".///../////utils/////email");
 //const errorController=require('.///errorController')
 
 const signToken=id=>{
@@ -101,19 +103,30 @@ exports.forgotPassword=catchAsync(async(req,res,next)=>{
     const resetToken=user.createPasswordResetToken();
     await user.save({validateBeforeSave:false});
     //3)Send it to user's email
-    // try{
-    //     const resetURL=`${req.protocol}://${req.get("host")}/api/v1/users/resetPassword/${resetToken}`;
-    //     await new Email(user,resetURL).sendPasswordReset();
+     try{
+         const resetURL=`${req.protocol}://${req.get("host")}/api/users/resetPassword/${resetToken}`;
+         const message=`Forgot your password? Submit a PATCH request with your new password and passwordConfirm to:${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+         await sendEmail({
+                email:user.email,
+                subject:"Your password reset token(valid for 10 min)",
+                message
+         });
+            res.status(200).json({
+                status:"success",
+                message:"Token sent to email"
+            });
+
+         //     await new Email(user,resetURL).sendPasswordReset();
     //     res.status(200).json({
     //         status:"success",
     //         message:"Token sent to email"
     //     });
-    // }catch(err){
-    //     user.passwordResetToken=undefined;
-    //     user.passwordResetExpires=undefined;
-    //     await user.save({validateBeforeSave:false});
-    //     return next(new AppError("There was an error sending the email. Try again later",500));
-    // }
+    }catch(err){
+        user.passwordResetToken=undefined;
+        user.passwordResetExpires=undefined;
+        await user.save({validateBeforeSave:false});
+        return next(new AppError("There was an error sending the email. Try again later",500));
+    }
 }
 );
 exports.resetPassword=catchAsync(async(req,res,next)=>{
