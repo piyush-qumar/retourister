f9ff01b2c1505e21ea889e9d0eb8ad4013135a67
piyush_qumar <piyushkumar5271@gmail.com>
2022-10-09 18:52:51 +0530

    updateme router defined along withfilterObj

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,7 +1,16 @@
 //const fs=require('fs');
 const User=require(".//..///models////userModel");
-const AppError=require("..///utils////AppError");
+const AppError=require(".//..//utils///AppError");
 const catchAsync=require("..///utils///catchAsync");
+
+const filterObj=(obj, ...allowedFields)=>{
+    const newObj={};
+    Object.keys(obj).forEach(el=>{
+        if(allowedFields.includes(el)) 
+        newObj[el]=obj[el];
+    })
+    return newObj;
+}
 
 exports.getAllUsers=catchAsync(async(req,res,next)=>{
     const user=await User.find();
@@ -14,6 +23,26 @@ exports.getAllUsers=catchAsync(async(req,res,next)=>{
         }
     })
 });
+exports.updateMe=catchAsync(async(req,res,next)=>{
+    // 1> error if user tries to update password in this route
+    if(req.body.password||req.body.passwordConfirm){
+        return next(new AppError("This route is not for password updates",400));
+    }
+    // 2> filtered out unwanted field names that are not allowed to be updated  
+    const flteredBody=filterObj(req.body,'name','email');
+    // 3> update user document
+    const updatedUser=await User.findByIdAndUpdate(req.user.id,flteredBody,{
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        status:"success",
+        data:{
+            user:updatedUser
+        }
+    })
+});
+
 exports.createUser=(req,res)=>{
     res.status(500).json({
         status:"error",
