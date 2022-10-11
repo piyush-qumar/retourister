e0df6bb4b014d167c14e6586264d46a86f6ab0ee
piyush_qumar <piyushkumar5271@gmail.com>
2022-10-11 23:08:34 +0530

    mergeparams

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -2,8 +2,12 @@ const express=require("express");
 const router=express.Router();
 const tourController=require("../controllers/tourController")
 const authController=require('./..//controllers///authController');
-const reviewController=require("./../controllers/reviewController")
+const reviewRouter=require("./reviewRoutes");
+router.use('/:tourId/reviews',reviewRouter);
+
+//const reviewController=require("./../controllers/reviewController")
 //router.param("id",tourController.checkID);
+
 router.route('/5-cheapest').get(tourController.aliass,tourController.getAllTours);
 router.route('/get-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
@@ -11,7 +15,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router.route("/").get(authController.protect,tourController.getAllTours).post(tourController.createTour);
 
 router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect,authController.restrictTo('admin','lead-guide'),tourController.deleteTour);
-router.route('/:tourId/reviews').post(authController.protect,authController.restrictTo('user'),reviewController.createReview);
+//router.route('/:tourId/reviews').post(authController.protect,authController.restrictTo('user'),reviewController.createReview);
 module.exports=router;
 
 
