3e46f349f36a6be6482e804d5b3ef7deb4512bbd
piyush_qumar <piyushkumar5271@gmail.com>
2022-10-29 14:40:04 +0530

    login page added

--- 4-natours/starter/controllers/viewController.js
+++ 4-natours/starter/controllers/viewController.js
@@ -11,7 +11,7 @@ exports.getOverview=catchAsync(async(req,res,next)=>{
         tours
     });
 })
-exports.getTour=catchAsync(async(req,res)=>{
+exports.getTour=catchAsync(async(req,res,next)=>{
     //1. get the data for the requested tour(including reviews and guides)
     const tour=await Tour.findOne({slug:req.params.slug}).populate({
         path:'reviews',
